0eecccc51b66e34a451ad81464bedcf5c9ccf1a3
seahawk1986 <seahawk1986@hotmail.com>
2014-01-29 23:27:06 +0100

    Update xine.py

--- frontends/xine.py
+++ frontends/xine.py
@@ -3,7 +3,6 @@ import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 import logging
-from pydbus2vdr.dbus2vdr import *
 from frontends.base import *
 import os
 import subprocess
@@ -12,9 +11,6 @@ class Xine():
     def __init__(self, main, name, path='/usr/bin/xine',origin='127.0.0.1',port='37890'):
         self.main = main
         self.name = name
-        self.main_instance.hdf.updateKey('yavdr.frontend.sxfe.autocrop','0') # Remove
-        self.main_instance.hdf.updateKey('yavdr.frontend.xine.anamorphic','') # Remove
-        self.main_instance.hdf.updateKey('yavdr.frontend.xine.autocrop','0') # Remove
         if self.main.settings.get_settingb('xine', 'autocrop', False):
             autocrop = "--post autocrop:enable_autodetect=1,enable_subs_detect=1,soft_start=1,stabilize=1"
         else:
@@ -29,22 +25,23 @@ class Xine():
         # TODO Display config:
         os.environ['__GL_SYNC_DISPLAY_DEVICE'] = os.environ['DISPLAY']
 
-        self.cmd = ['/usr/bin/xine --post tvtime:method=use_vo_driver \
+        self.cmd = '''/usr/bin/xine --post tvtime:method=use_vo_driver \
             --config /etc/xine/config \
             --keymap=file:/etc/xine/keymap \
             --post vdr --post vdr_video --post vdr_audio --verbose=2 \
             --no-gui --no-logo --no-splash --deinterlace -pq \
             -A pulseaudio \
             %s %s \
-            vdr:/tmp/vdr-xine/stream#demux:mpeg_pes'%(autocrop, aspectratio)]
+            vdr:/tmp/vdr-xine/stream#demux:mpeg_pes'''%(autocrop, aspectratio)
         self.proc = None
         self.environ = os.environ
         logging.debug(' '.join(self.cmd))
 
     def attach(self):
         logging.debug('starting xine')
+        logging.debug('self.cmd')
         self.proc = subprocess.Popen("exec " + self.cmd,
-                                     shell=True,env=os.environ)
+                                     shell=True, env=os.environ)
         logging.debug('started xine')
 
     def detach(self,active=0):
